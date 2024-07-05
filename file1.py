4f4a032aeb6724299ec0a19f8ea7e060a289ec43
JohnChesterTayas <tayasjohnchester@gmail.com>
2024-07-05 11:57:56 +0800

    Update file1.py

--- file1.py
+++ file1.py
@@ -16,3 +16,13 @@ def fibonacci_sequence(n):
 
 result = fibonacci_sequence(10)
 print("Fibonacci sequence up to 10 numbers: {result}") 
+
+# file1.py
+def newfactorial(n):
+if n == 0:
+    return 1
+else:
+    return n * factorial(n-1)
+
+result = factorial(5)
+print("Factorial of 5 is: {result}")
